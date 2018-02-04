33efbccd6bfd62d35037297c0eafc244445f051a
Maciej Lorenc <lorencmaciek@gmail.com>
2018-02-04 01:10:34 +0100

    Changed function used to send email

--- src/contact_form/views.py
+++ src/contact_form/views.py
@@ -1,19 +1,12 @@
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.sites.shortcuts import get_current_site
-from django.core.mail import EmailMessage
+from django.core.mail import send_mail
 from django.shortcuts import render
 
 from decouple import config
 
 from .forms import ContactForm
-
-
-def send_email(from_email, to_emails, subject, message):
-    headers = {'Reply-To': from_email, 'From': 'contact form'}
-    email = EmailMessage(subject, message, to=to_emails,
-                         headers=headers)
-    email.send()
 
 
 def contact_page(request):
@@ -25,7 +18,7 @@ def contact_page(request):
             to_emails = [settings.CONTACT_FORM_EMAIL]
             subject = '{} contact form'.format(domain)
             message = form.cleaned_data['message']
-            send_email(from_email, to_emails, subject, message)
+            send_mail(subject, message, from_email, to_emails, fail_silently=False)
             messages.success(request, 'Message sent!')
         else:
             messages.warning(request, 'There was a problem! Please try again.')
